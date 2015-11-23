459a21aea6d69a693f4135ca2a915ad763247ca4
liululu <443236745@qq.com>
2015-11-23 16:01:18 +0800

    first

---
+++ src/main/java/com/liululu/demoLuceneSimple/App.java
@@ -0,0 +1,13 @@
+package com.liululu.demoLuceneSimple;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
